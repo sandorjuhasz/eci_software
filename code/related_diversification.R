ced091fc73c046008d212176e2fb4366f5b58694
sandorjuhasz <juhasz.sandor@krtk.mta.hu>
2023-11-24 15:14:00 +0100

    update w/ new EconGeo version

--- code/related_diversification.R
+++ code/related_diversification.R
@@ -18,11 +18,11 @@ relatedness <- as.matrix(get.adjacency(graph.data.frame(ssel)))
 cdf <- fread("../outputs/complexity_table2020.csv")
 cdf$rca01 <- ifelse(cdf$rca >= 1, 1, 0)
 mcp <- select(cdf, iso2_code, language, rca01)
-mcp_mat <- EconGeo::get.matrix(mcp)
+mcp_mat <- EconGeo::get_matrix(mcp)
 
 
 # relatedness density
-rel_density <- relatedness.density(mcp_mat, relatedness)
+rel_density <- EconGeo::relatedness_density(mcp_mat, relatedness)
 rel_density <- data.table(Melt(rel_density))
 colnames(rel_density) <- c("country", "language", "rel_density")
   
@@ -59,6 +59,7 @@ reg_df$entry <- ifelse(reg_df$rca01==0 & reg_df$rca01_2023==1, 1, 0)
 
 summary(m1 <- lm(entry ~ rel_density, data = reg_df))
 summary(m2 <- lm(entry ~ rel_density + pci, data = reg_df))
+summary(m2_fe <- lm(entry ~ rel_density + as.factor(iso2_code), data = reg_df))
 summary(m2_fe <- lm(entry ~ rel_density + pci + as.factor(iso2_code), data = reg_df))
 
 stargazer(
