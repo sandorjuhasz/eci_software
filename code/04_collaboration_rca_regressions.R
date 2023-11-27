7f021f0f88dee8eab86417316d2cecbc86fea9bb
Johannes Wachs <johanneswachs@Johanness-MacBook-Pro.local>
2023-11-27 10:24:08 +0100

    interaction regression models now using fixest R library and DK HAC robust SE

--- code/04_collaboration_rca_regressions.R
+++ code/04_collaboration_rca_regressions.R
@@ -7,46 +7,42 @@ library(dplyr)
 library(igraph)
 library(mefa4)
 library(stargazer)
+library(fixest)
 
 
-fdf=fread('../outputs/relatedness_country_semester_panel_with_collab_neighbors.csv')
+fdf=fread('/Users/johanneswachs/Dropbox/majorprojects/empirical_software/github_innovation_graph/git_complexity/outputs/relatedness_country_semester_panel_with_collab_neighbors.csv')
 
 head(fdf)
 
-summary(m1 <- lm(rca_entry ~ rel_density + neighbor_related, data = fdf))
-summary(m1_fe <- lm(rca_entry ~ rel_density +neighbor_related + as.factor(iso2_code), data = fdf))
-summary(m2 <- lm(rca_entry ~ rel_density + pci + neighbor_related, data = fdf))
-summary(m2_fe <- lm(rca_entry ~ rel_density + pci +neighbor_related + as.factor(iso2_code), data = fdf))
-summary(m2I <- lm(rca_entry ~ rel_density + pci + neighbor_related+rel_density:neighbor_related, data = fdf))
-summary(m2I_fe <- lm(rca_entry ~ rel_density + pci +neighbor_related +rel_density:neighbor_related+
-                      as.factor(iso2_code), data = fdf))
+m1<-feols(rca_entry ~ rel_density | iso2_code +language + semester_id,
+          panel.id=~iso2_code+semester_id,
+          data=fdf,
+          se="DK")
+m2<-feols(rca_entry ~ neighbor_related | iso2_code +language + semester_id,
+          panel.id=~iso2_code+semester_id,
+          data=fdf,
+          se="DK")
+m3<-feols(rca_entry ~ rel_density+neighbor_related | iso2_code +language + semester_id,
+          panel.id=~iso2_code+semester_id,
+          data=fdf,
+          se="DK")
 
-summary(m3_fe <- lm(rca_entry ~ rel_density + pci +neighbor_related +rel_density:neighbor_related+
-                      as.factor(iso2_code) + as.factor(language), data = fdf))
-summary(m4_fe <- lm(rca_entry ~ rel_density + pci +neighbor_related +rel_density:neighbor_related+
-                      as.factor(iso2_code) + as.factor(language) +
-                      as.factor(semester_id), data = fdf))
+m4<-feols(rca_entry ~ rel_density+neighbor_related + pci | iso2_code + semester_id,
+          panel.id=~iso2_code+semester_id,
+          data=fdf,
+          se="DK")
 
-stargazer(
-  m1,
-  m1_fe,
-  m2,
-  m2_fe,
-  m2I,
-  m2I_fe,
-  m3_fe,
-  m4_fe,
-  digits = 4,
-  omit = c("iso2_code", "language", "semester_id"),
-  add.lines=list(
-    c("Country FE", "No", "Yes", "No", "Yes","No","Yes", "Yes", "Yes"),
-    c("Language FE", "No", "No", "No", "No","No","No", "Yes", "Yes"),
-    c("Period FE", "No", "No", "No", "No","No","No", "No", "Yes")
-  ),
-  #type="text"
-  out = "../outputs/interaction_model_v1.html"
-)
+m5<-feols(rca_entry ~ rel_density+neighbor_related+rel_density:neighbor_related | iso2_code +language + semester_id,
+          panel.id=~iso2_code+semester_id,
+          data=fdf,
+          se="DK")
 
+m6<-feols(rca_entry ~ rel_density+neighbor_related+rel_density:neighbor_related +pci | iso2_code + semester_id,
+          panel.id=~iso2_code+semester_id,
+          data=fdf,
+          se="DK")
+
+etable(m1,m2,m3,m4,m5,m6,file = '../johanneswachs/Dropbox/majorprojects/empirical_software/github_innovation_graph/git_complexity/outputs/interaction_model_v2.html')
 
 
 
