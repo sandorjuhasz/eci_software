327174d72e92e5695a71b1ac3125e848e027da2b
Jermain Kaminski <jermain.kaminski@gmail.com>
2024-03-15 08:46:02 +0100

    Clusters

--- code/08_entry_regressions_clusters.R
+++ code/08_entry_regressions_clusters.R
@@ -14,36 +14,23 @@ df <- fread("../outputs/data_entry_regression_2022_to_2023_communities.csv")
 df$rel_density <- scale(df$rel_density)
 df$pci <- scale(df$pci)
 
-# Group names
-group_names <- c("Machine_Learning_Embedded_Systems", 
-                 "Scripting_Automation", 
-                 "Specialized_Research", 
-                 "Systems_Low_Level_Programming", 
-                 "Web_Application_Development")
-
-# Create dummy variables for the 'group' variable, without excluding one as reference
-group_dummies <- model.matrix(~ group - 1, data = df, colnames = group_names)
-df <- cbind(df, group_dummies)
+df$group <- factor(df$group, levels = c("Systems_Low_Level_Programming", "Machine_Learning_Embedded_Systems", "Scripting_Automation", "Specialized_Research", "Web_Application_Development"))
 
 # models
-summary(m1 <- lm(entry01 ~ rel_density, data = df))
-summary(m1_fe <- lm(entry01 ~ rel_density + as.factor(iso2_code), data = df))
-summary(m1_fe2 <- lm(entry01 ~ rel_density + as.factor(language), data = df))
-summary(m2 <- lm(entry01 ~ rel_density + pci, data = df))
-summary(m2_fe <- lm(entry01 ~ rel_density + pci + as.factor(iso2_code), data = df))
-summary(m3_fe2 <- lm(entry01 ~ rel_density + pci + as.factor(iso2_code) +  group_dummies, data = df))
+summary(m1 <- lm(entry01 ~ as.factor(iso2_code) + as.factor(group), data = df))
+summary(m2 <- lm(entry01 ~ rel_density + as.factor(iso2_code) + as.factor(group), data = df))
+summary(m3 <- lm(entry01 ~ rel_density + pci + as.factor(iso2_code) + as.factor(group), data = df))
+
+levels(df$group)
 
 stargazer(
   m1,
-  m1_fe,
-  m1_fe2,
   m2,
-  m2_fe,
-  m3_fe2,
+  m3,
   omit = c("iso2_code", "language"),
   add.lines=list(
-    c("Country FE", "No", "Yes", "No", "No", "Yes", "Yes"),
-    c("Language FE", "No", "No", "Yes", "No", "No", "No")
+    c("Country FE", "Yes", "Yes", "Yes"),
+    c("Language FE", "No", "No", "Yes")
   ),
-  out = "../outputs/new_entry_2022_2023_communities.html"
+  out = "../outputs/new_entry_2022_2023_clusters.html"
 )
