ead082a30156b999d24a659f0ce01b98e60eabfc
sandorjuhasz <juhasz.sandor@krtk.mta.hu>
2024-03-28 16:58:04 +0100

    emission models --updated

--- code/04_eci_regressions.R
+++ code/04_eci_regressions.R
@@ -17,6 +17,7 @@ df <- fread("../outputs/eci_comparisons_2020.csv")
 # manipulation
 df$log_gpd_pc <- log10(df$gdpcap_o)
 df$gini_norm <- scale(df$gini_mean)
+df$emission_norm <- scale(df$embodied_emissions)
 df$software_eci_norm <- scale(df$software_eci_2020)
 df$trade_eci_norm <- scale(df$trade_eci_2020)
 df$tech_eci_norm <- scale(df$tech_eci_2020)
@@ -89,4 +90,37 @@ stargazer(
   out = "../outputs/table2_gini_eci_regressions.html"
 )
 
+
+
+
+### emissions vs ECI
+# drop rows w/ NAs in key columns
+key_columns <- c("emission_norm", "log_gpd_pc", "software_eci_norm", "trade_eci_norm", "tech_eci_norm", "research_eci_norm", "log_pop")
+reg_df <- df[complete.cases(df[, ..key_columns]), ]
+
+# models
+summary(m1 <- lm(emission_norm ~ software_eci_norm + log_gpd_pc + log_pop, data = reg_df))
+summary(m2 <- lm(emission_norm ~ trade_eci_norm + log_gpd_pc + log_pop, data = reg_df))
+summary(m3 <- lm(emission_norm ~ tech_eci_norm + log_gpd_pc + log_pop, data = reg_df))
+summary(m4 <- lm(emission_norm ~ research_eci_norm + log_gpd_pc + log_pop, data = reg_df))
+summary(m5a <- lm(emission_norm ~ software_eci_norm + trade_eci_norm + log_gpd_pc + log_pop, data = reg_df))
+summary(m5b <- lm(emission_norm ~ software_eci_norm + tech_eci_norm + log_gpd_pc + log_pop, data = reg_df))
+summary(m5c <- lm(emission_norm ~ software_eci_norm + research_eci_norm + log_gpd_pc + log_pop, data = reg_df))
+summary(m6 <- lm(emission_norm ~ software_eci_norm + trade_eci_norm + tech_eci_norm + research_eci_norm + log_gpd_pc + log_pop, data = reg_df))
+
+stargazer(
+  m1,
+  m2,
+  m3,
+  m4,
+  m5a,
+  m5b,
+  m5c,
+  m6,
+  omit.stat=c("f", "ser"),
+  dep.var.caption = "",
+  dep.var.labels = c("Emission"),
+  covariate.labels = c("ECI software", "ECI trade", "ECI technology", "ECI research", "GPD per capita (log)", "Population (log)"),
+  out = "../outputs/table3_emission_eci_regressions.html"
+)
 
