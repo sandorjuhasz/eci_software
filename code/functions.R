dbdf569c1ea51d716165315d83cb36c02343b19a
sandorjuhasz <juhasz.sandor@krtk.mta.hu>
2025-03-02 14:12:34 +0100

    functions for SI regressions

--- code/functions.R
+++ code/functions.R
@@ -5,6 +5,45 @@ library(officer)
 
 
 
+
+
+
+# for the baseline table to produce most regressions
+create_baseline_table <- function(input_path) {
+  df <- fread(input_path)
+  
+  # manipulation
+  df <- df %>%
+    group_by(year) %>%
+    mutate(
+      log_gdp_usd = log10(gdp_current_USD),
+      log_gdp_ppp = log10(gdp_ppp),
+      gdp_ppp_pc = gdp_ppp / population,
+      log_gdp_ppp_pc = log10(gdp_ppp_pc),
+      log_gdp_ppp_pc2 = log_gdp_ppp_pc^2,
+      gini_norm = scale(gini_mean),
+      gini_2020_2022_norm = scale(gini_mean_2020_2022),
+      log_emission = log10(total_ghg_emissions),
+      emission_per_gdp = (total_ghg_emissions / gdp_ppp),
+      log_emission_per_gdp = log10(total_ghg_emissions / gdp_ppp),
+      eci_software_norm = scale(eci_software),
+      eci_trade_norm = scale(eci_trade),
+      eci_tech_norm = scale(eci_tech),
+      eci_research_norm = scale(eci_research),
+      log_pop = log10(population),
+      log_nat_res = log10(natural_resources)
+    ) %>%
+    data.table()
+  
+  return(df)
+}
+
+
+
+
+
+
+# to save etable regression table as a docx
 save_etable_to_word <- function(etable_output, file_name = "../outputs/etable_output.docx") {
   # convert etable output to a dataframe
   etable_df <- as.data.frame(etable_output)
